ad1c215f7c0fc3386ff6fc9f8bef5ae2476f63b7
Example: Riley Murphy <rileycmurph@outlook.com>
2022-08-22 19:40:13 -0400

    created sentence with variables

--- Program.cs
+++ Program.cs
@@ -2,11 +2,16 @@
 
 namespace VariablesCS
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            int numberOfCupsOfCoffee = 2;
+            var fullName = "Riley Murphy";
+            var today = "August 22nd, 2022";
+            Console.WriteLine($"Today, {today}, {fullName} drank {numberOfCupsOfCoffee} cups of coffee.");
+            Console.WriteLine("Today I drank " + numberOfCupsOfCoffee + " cups of coffee.");
+
+        }
     }
-  }
 }
