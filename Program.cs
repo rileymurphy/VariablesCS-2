a0074db75d9c95fd17392d7921328a53c4d2c5af
Example: Riley Murphy <rileycmurph@outlook.com>
2022-08-24 20:30:18 -0400

    Variables hw COMPLETE!!

--- Program.cs
+++ Program.cs
@@ -6,11 +6,46 @@ namespace VariablesCS
     {
         static void Main(string[] args)
         {
-            int numberOfCupsOfCoffee = 2;
+            var numberOfCupsOfCoffee = 2;
             var fullName = "Riley Murphy";
-            var today = "August 22nd, 2022";
-            Console.WriteLine($"Today, {today}, {fullName} drank {numberOfCupsOfCoffee} cups of coffee.");
-            Console.WriteLine("Today I drank " + numberOfCupsOfCoffee + " cups of coffee.");
+            var today = DateTime.Now;
+            Console.WriteLine($"As of {today}, {fullName} drank {numberOfCupsOfCoffee} cups of coffee.");
+
+            Console.WriteLine("What is your name?");
+            var userName = Console.ReadLine();
+
+            if (userName == "Alice")
+            {
+                Console.WriteLine($"Haayyy Alice!");
+            }
+            else
+            {
+                Console.WriteLine($"INTRUDER ALERT!!!");
+            }
+
+            Console.WriteLine("Enter first number.");
+            var firstNumberAsString = Console.ReadLine();
+            var firstOperand = double.Parse(firstNumberAsString);
+
+            Console.WriteLine("Enter second number.");
+            var secondNumberAsString = Console.ReadLine();
+            var secondOperand = double.Parse(secondNumberAsString);
+
+            var sum = firstOperand + secondOperand;
+            Console.WriteLine($"The sum of these numbers is {sum}. ");
+
+            var difference = firstOperand - secondOperand;
+            Console.WriteLine($"The difference of {firstOperand} and {secondOperand} is {difference}. ");
+
+            var product = firstOperand * secondOperand;
+            Console.WriteLine($"The product of these numbers is {product}. ");
+
+            var quotient = firstOperand / secondOperand;
+            Console.WriteLine($"The quotient of {firstOperand} and {secondOperand} is {quotient}.");
+
+            var remainder = firstOperand % secondOperand;
+            Console.WriteLine($"The remainder of {firstOperand} and {secondOperand} is {remainder}. ");
+
 
         }
     }
